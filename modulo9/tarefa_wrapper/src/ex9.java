313da549411a446b3e78903b3f3f52da109148d3
Pedro <pedro.salomao@k2pr.com.br>
2023-05-31 20:49:54 -0300

    ex9

---
+++ modulo9/tarefa_wrapper/src/ex9.java
@@ -0,0 +1,8 @@
+public class ex9 {
+    public static void main(String[] args) {
+        double nove = 9.74;
+        Double novew = nove;
+        System.out.println("numero: " + novew);
+    }
+    
+}
